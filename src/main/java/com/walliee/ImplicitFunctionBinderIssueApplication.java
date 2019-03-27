6a8aee9fecb477d9f2e845ee3838eff5437a595d
Walliee <ansh.mehra@me.com>
2019-03-27 00:47:57 -0400

    Initial commit

---
+++ src/main/java/com/walliee/ImplicitFunctionBinderIssueApplication.java
@@ -0,0 +1,41 @@
+package com.walliee;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Supplier;
+
+@SpringBootApplication
+public class ImplicitFunctionBinderIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplicitFunctionBinderIssueApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner runner() {
+		return args -> {
+			while (true) {
+				System.out.println("do something");
+				Thread.sleep(1000L);
+			}
+		};
+	}
+
+	@Component
+	public static class StringSuppiler implements Supplier<String> {
+		private String field;
+
+		public StringSuppiler(String field) {
+			this.field = field;
+		}
+
+		@Override
+		public String get() {
+			return null;
+		}
+	}
+}
